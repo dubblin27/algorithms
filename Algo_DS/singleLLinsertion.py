00962dec95d8bb91192146a117dbfca172fdaf4d
Sabrish Kumar P <58567129+dubblin27@users.noreply.github.com>
2020-04-07 03:08:09 +0530

    LLcommit

--- Algo_DS/singleLLinsertion.py
+++ Algo_DS/singleLLinsertion.py
@@ -23,32 +23,45 @@ class Linked_List:
         while currentnode:
             print(currentnode.data)
             currentnode = currentnode.next
+
     def append_start(self,data):
         newnode = node(data)
         newnode.next = self.head 
         self.head = newnode
-    def append_middle(self,prev_node,data):
-        prev_node= node(prev_node)
-        print ("prev_node.data : ",prev_node.data)
+
+    def append_middle(self,dataa,data):
+        prev_node = node(dataa)
+        xnode = self.head 
+        count = 0
+        while xnode :
+            if prev_node.data == xnode.data :
+                prev_node = xnode
+                break
+            xnode = xnode.next
         if not prev_node:
             print("not in list")
-            return
-        currentnode = self.head 
-        while currentnode:
-            if currentnode.data == prev_node.data:
-                print("currentnode.data : ",currentnode.data)
-                currentnode = prev_node
-            currentnode = currentnode.next
-        
+            return 
         newnode = node(data)
-        print("newnode.data : ",newnode.data)
         newnode.next = prev_node.next 
         prev_node.next = newnode
+
+#driver_code
 ll = Linked_List()
-ll.append(1)
-ll.append(2)
-# ll.printll()
-ll.append_start(3)
-print("head : ",ll.head.data)
-ll.append_middle(1,5)
-ll.printll()
+while True : 
+    print("\n1.append at end\n2.append at start\n3.append at middle\n4.print")
+    option = int(input("enter an option : "))
+    if option == 1 :
+        data = int(input("enter value to append : "))
+        ll.append(data)
+    if option == 2 :
+        data = int(input("enter value to append : "))
+        ll.append_start(data)
+    if option == 3 :
+        data = int(input("enter current value  : "))
+        data1 = int(input("enter value to append  : "))
+        ll.append_middle(data,data1)
+    if option == 4 :
+        ll.printll()
+
+
+    
